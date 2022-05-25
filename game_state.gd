f5b345e2f2941f50a19f67e1a685d8561d8a9ff6
Josejulio Martínez <josejulio@users.noreply.github.com>
2022-05-25 10:20:08 -0600

    Basic game state saving function  - Loads/save to file and sets back the current game state (Stats)  - Need to expand as we have more state we want to save

--- game_state.gd
+++ game_state.gd
@@ -21,3 +21,28 @@ func set_health(value):
 	emit_signal("health_changed", player_stats.health)
 	if player_stats.health <= 0:
 		emit_signal("no_health")
+
+func save_game(filename: String):
+	var save_data: Dictionary = {
+		player = {
+			stats = player_stats
+		}
+	}
+
+	var file = File.new()
+	file.open(filename, File.WRITE)
+	file.store_var(save_data, true)
+	file.close()
+
+func load_game(filename: String):
+	var file = File.new()
+	if file.file_exists(filename):
+		file.open(filename, File.READ)
+		var game_data = file.get_var()
+		file.close()
+		
+		player_stats = game_data.player.stats
+
+		return true
+	else:
+		return false
