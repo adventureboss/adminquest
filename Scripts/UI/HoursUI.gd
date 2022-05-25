294bd3e82c694b030bc008e5ccd6977bb8023c6d
justinorringer <justinorringer@gmail.com>
2022-05-25 16:04:45 -0400

    Removed print statements

--- Scripts/UI/HoursUI.gd
+++ Scripts/UI/HoursUI.gd
@@ -6,10 +6,8 @@ onready var hoursUI = $HoursLabel
 
 func display_hours(value):
 	if hoursUI != null:
-		print("thing")
 		hoursUI.text = str(value)
 	
 func _ready():
-	print("thing1")
 	display_hours(game_state.player_stats.hours)
 	game_state.connect("hours_changed", self, "display_hours")
