7129309dcd83b5f4fd2feea54fdab50647c3e048
Stephen Adams <tsadams@gmail.com>
2022-05-29 11:42:42 -0400

    add an introduction scene
    YAY
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

---
+++ Scripts/FadeOut.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+signal fade_finished
+
+func Fade_out():
+	$AnimationPlayer.play("fade_out")
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
+	emit_signal("fade_finished")
