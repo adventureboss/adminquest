2484e25c6ad8f4d90b693b9293132eb55ab6938c
justinorringer <justinorringer@gmail.com>
2022-05-30 16:42:05 -0400

    Added win_state clause and adition dialogFile var

--- Scripts/InteractableObjects/Doug.gd
+++ Scripts/InteractableObjects/Doug.gd
@@ -1,5 +1,1 @@
 extends "Npc.gd"
-
-
-func _on_Area2D_area_exited(area):
-	pass # Replace with function body.
--- Scripts/InteractableObjects/Npc.gd
+++ Scripts/InteractableObjects/Npc.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
 export(Resource) var dialogFile
+export(Resource) var dialogFile_completed
 export(String) var entryDialog
 
 func interact():
-	GameState.start_dialogue(dialogFile, entryDialog)
+	if GameState.win_status && is_instance_valid(dialogFile_completed):
+		GameState.start_dialogue(dialogFile_completed, entryDialog)
+	else:
+		GameState.start_dialogue(dialogFile, entryDialog)
